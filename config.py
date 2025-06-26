1a9f31acfc538b7a24def1f38554930cb2b6555e
TheAcheronia <acheronia@proton.me>
2025-06-26 21:50:13 +0530

    Create config.py
    Replace <YOUR_GEMINI_API> and <YOUR_OPENWEATHER_API with respective API keys

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+# This file stores your API keys.
+# IMPORTANT: Keep this file private and do not share it.
+
+# Get your API key from Google AI Studio: https://aistudio.google.com/app/apikey
+GEMINI_API_KEY = "YOUR_GEMINI_API"
+
+# Get your API key from OpenWeatherMap: https://home.openweathermap.org/api_keys
+OPENWEATHER_API_KEY = "YOUR_OPENWEATHER_API"
