5db591e4da893ff34d59344ade8935fbebbcfc21
TheAcheronia <acheronia@proton.me>
2025-06-26 22:13:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,5 @@ GEMINI_API_KEY = "YOUR_GEMINI_API"
 
 # Get your API key from OpenWeatherMap: https://home.openweathermap.org/api_keys
 OPENWEATHER_API_KEY = "YOUR_OPENWEATHER_API"
+
+# Replace YOUR_GEMINI_API and YOUR_OPENWEATHER_API with their respective API keys
